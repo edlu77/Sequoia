8311510c8a329bed6922a89b1b07dd8b3cfa9dc9
edlu77 <eddd.lu@gmail.com>
2018-11-13 18:15:43 -0500

    fix syntax

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -13,12 +13,13 @@ class Api::SessionsController < ApplicationController
   end
 
   def destroy
-  @user = current_user
-  if @user
-    logout
-    render "api/users/show"
-  else
-    render json: ["Nobody signed in"], status: 404
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render json: ["Nobody signed in"], status: 404
+    end
   end
 
 end
