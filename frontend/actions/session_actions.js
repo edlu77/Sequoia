2a0da8b9e2c0eaae77c114b4fa6227d96de9c759
edlu77 <eddd.lu@gmail.com>
2018-11-13 17:12:08 -0500

    begin front end auth

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,27 @@
+import * as SessionApiUtil from '../util/session_api_util';
+export RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export LOGOUT_USER = 'LOGOUT_CURRENT_USER'
+export RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+
+const receiveCurrentUser = (currentUser) => {
+  return({
+    type: "RECEIVE_CURRENT_USER",
+    currentUser: currentUser,
+  })
+}
+
+const logoutCurrentUser = () => {
+  return({
+    type: "LOGOUT_CURRENT_USER",
+  })
+}
+
+const receiveErrors = (errors) => {
+  return({
+    type: "RECEIVE_ERRORS",
+    errors: errors,
+  })
+}
+
+export const
