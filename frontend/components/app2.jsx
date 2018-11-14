697677ce7d4029a2101f1a2601933cdef0a80ce3
edlu77 <eddd.lu@gmail.com>
2018-11-14 14:39:50 -0500

    add app2.jsx

--- frontend/components/app2.jsx
+++ frontend/components/app2.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import LoginFormContainer from './login_form_container';
+import SignupFormContainer from './signup_form_container';
 
 const App = () => {
   return (
     <div>
       <h1>Sequoia</h1>
+      <Route exact path="/" component = {LoginFormContainer} />
+      <Route path="/login" component={LoginFormContainer} />
+      <Route path="/signup" component={SignupFormContainer} />
     </div>
   );
 };
