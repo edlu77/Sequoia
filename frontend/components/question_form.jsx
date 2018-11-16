5afc501f8de34b1f43eb5209e791389fbf3715f8
edlu77 <eddd.lu@gmail.com>
2018-11-16 11:32:09 -0500

    change question files

---
+++ frontend/components/question_form.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class QuestionForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = this.props.question
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  update(field) {
+    return (e) => {
+      this.setState({[field]: e.target.value});
+    };
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.submitAction(this.state)
+  };
+
+  render () {
+    debugger
+    return (
+      <div className="question-form">
+        <h3>{this.props.formType}</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>Your Question
+            <input
+              type="text"
+              value={this.state.title}
+              onChange={this.update('title')} />
+          </label>
+          <br></br>
+          <label>Topic
+            <input
+              type="text"
+              value={this.state.topic}
+              onChange={this.update('topic')} />
+          </label>
+
+          <input type="submit" value={this.props.formType} />
+        </form>
+        <Link to='/'>Back to main page</Link>
+      </div>
+    );
+  };
+};
+
+export default QuestionForm;
